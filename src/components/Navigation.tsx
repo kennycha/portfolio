adcddc42aa9ac099cc5f7d51084dc5dcd6f733f2
kennycha <dudqn136@naver.com>
2025-08-29 23:35:04 +0900

    chore: 글자색 변경

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,7 +4,7 @@ export const Navigation = () => {
   return (
     <nav className="pointer-events-none fixed top-0 right-0 left-0 z-50 flex items-center justify-center bg-transparent py-6 md:py-12">
       <div className="pointer-events-auto flex items-center space-x-2 rounded-full border border-gray-300 bg-white/80 px-6 py-2 shadow-sm backdrop-blur-sm md:space-x-8">
-        <p className="text-2xl font-bold text-blue-800">Kennycha</p>
+        <p className="text-2xl font-bold text-gray-800">Kennycha</p>
         <Link to="/" className="text-gray-600 transition-colors hover:text-gray-900">
           Projects
         </Link>
